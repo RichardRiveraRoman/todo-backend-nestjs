664e7f3489faa3783770fad6af30ee38be4a738d
RichardRiveraRoman <richard.rivera.roman+github@gmail.com>
2025-07-04 03:29:08 -0400

    feat(todos): configure module with TypeORM
    - Import TypeOrmModule.forFeature([Todo]) in TodosModule
    - Set up dependency injection for Todos repository
    - Prepare module for CRUD operations

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Todo } from './entities/todo.entity';
 
 @Injectable()
-export class TodosService {}
+export class TodosService {
+  constructor(
+    @InjectRepository(Todo)
+    private todosRepository: Repository<Todo>,
+  ) {}
+
+  // TODO: Create a DTO for CreateTodo
+  create(createTodoDto: CreateTodoDto) {
+    return 'This action adds a new todo';
+  }
+
+  findAll() {
+    return `This action returns all todos`;
+  }
+
+  findOne(id: number) {
+    return `This action returns the todo with ID: #${id}`;
+  }
+
+  // TODO: Create a DTO for UpdateTodo
+  update(id: number, updateTodoDto: UpdateTodoDto) {
+    return `This action updates the todo with ID: #${id}`;
+  }
+
+  remove(id: number) {
+    return `This action removes the todo with ID: #${id}`;
+  }
+}
